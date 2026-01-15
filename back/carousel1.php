62ee45034ec531967acf7a78be1f4f3cb32d08d7
konekohip <konekohip@tuta.io>
2026-01-15 16:32:09 +0800

    commit

---
+++ back/carousel1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>set_carousel1_img</title>
+
+    <style>
+        .container1 {
+            width: 80%;
+            height: 50vh;
+            border: 1px black solid;
+            margin: auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="container1">
+        <form >
+    </div>
+</body>
+</html>
