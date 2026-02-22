d0067087acafea8cd80b0584b00a0c1e2d8b5aed
konekohip <konekohip@tuta.io>
2026-02-22 17:45:54 +0800

    更新sql

--- game_store_12_db.sql
+++ game_store_12_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2026-02-12 08:00:23
+-- 產生時間： 2026-02-22 10:38:12
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `boxes` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `text` varchar(20) NOT NULL,
+  `id` int(10) NOT NULL,
+  `text` text NOT NULL,
   `img` text NOT NULL,
-  `sh` int(1) UNSIGNED NOT NULL
+  `sh` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -39,10 +39,11 @@ CREATE TABLE `boxes` (
 --
 
 INSERT INTO `boxes` (`id`, `text`, `img`, `sh`) VALUES
-(1, 'LET IT DIE', 'game1.png', 1),
-(2, 'DQ1+2', 'game2.png', 1),
-(3, '無盡傳奇', 'game3.png', 1),
-(5, '死亡擱淺2', 'game8.png', 1);
+(13, 'DQ1+2', 'game2.png', 1),
+(14, 'LID', 'game1.png', 1),
+(16, 'TOX', 'game3.png', 1),
+(17, '塊魂', 'game4.png', 1),
+(18, '', 'game5.png', 1);
 
 -- --------------------------------------------------------
 
@@ -51,10 +52,10 @@ INSERT INTO `boxes` (`id`, `text`, `img`, `sh`) VALUES
 --
 
 CREATE TABLE `carousel` (
-  `id` int(20) UNSIGNED NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
   `text` text NOT NULL,
-  `img` text NOT NULL,
-  `thumb` text NOT NULL,
+  `img` varchar(20) NOT NULL,
+  `thumb` varchar(20) NOT NULL,
   `sh` int(1) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -63,10 +64,10 @@ CREATE TABLE `carousel` (
 --
 
 INSERT INTO `carousel` (`id`, `text`, `img`, `thumb`, `sh`) VALUES
-(1, '神機迷網', 'psstore3.png', 'psstore3-l.png', 1),
-(2, '年度回顧', 'psstore5.png', 'psstore5-l.png', 1),
-(3, '馬拉松', 'psstore1.png', 'psstore1-l.png', 1),
-(6, '羊蹄山戰鬼', 'psstore2.png', 'psstore2-l.png', 1);
+(2, '羊蹄山戰鬼', 'psstore2.png', 'psstore2-l.png', 1),
+(3, 'U惠', 'psstore4.png', 'psstore4-l.png', 1),
+(4, '年度回顧', 'psstore1.png', 'psstore1-l.png', 1),
+(7, '年度回顧', 'psstore5.png', 'psstore5-l.png', 1);
 
 -- --------------------------------------------------------
 
@@ -75,10 +76,10 @@ INSERT INTO `carousel` (`id`, `text`, `img`, `thumb`, `sh`) VALUES
 --
 
 CREATE TABLE `nav` (
-  `id` int(10) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
   `text` text NOT NULL,
   `sh` int(1) NOT NULL,
-  `main_id` int(10) NOT NULL
+  `main_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -86,11 +87,15 @@ CREATE TABLE `nav` (
 --
 
 INSERT INTO `nav` (`id`, `text`, `sh`, `main_id`) VALUES
-(1, 'Games', 1, 0),
-(2, 'PS5', 1, 1),
+(1, '456', 1, 0),
 (3, 'PS4', 1, 1),
 (4, 'PS5', 1, 0),
-(5, 'PS5', 1, 4);
+(5, '遊戲', 1, 4),
+(6, '控制器', 1, 4),
+(25, '13', 1, 1),
+(26, '233', 1, 1),
+(27, '23', 1, 4),
+(28, '456', 1, 24);
 
 --
 -- 已傾印資料表的索引
@@ -122,19 +127,19 @@ ALTER TABLE `nav`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `boxes`
 --
 ALTER TABLE `boxes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `carousel`
 --
 ALTER TABLE `carousel`
-  MODIFY `id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `nav`
 --
 ALTER TABLE `nav`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
